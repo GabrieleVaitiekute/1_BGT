8cd8ceecd808c211526a9a3426894b5c63f0d418
Gabriele Vaitiekute <gabriele.vaitiekute5@gmail.com>
2024-09-15 20:11:53 +0300

    updated main

--- main.cpp
+++ main.cpp
@@ -13,17 +13,29 @@ int main()
 	string input, output;
 	cout << "Iveskite norima teksta: ";
 	getline(cin, input);
+	string hash;
 	for (char a : input)
 	{
-		char newh;
-		if(!(a >= 'a' && a <= 'z' || a >= '0' && a <= '9'))
+		char new1, new2, new3, new4;
+		if(!(a >= 'a' && a <= 'z'))
 		{	
 			a = 'b';
 		}
-		
-		newh = a + 2;
-		output += newh;
+		if(!(a >= '0' && a <= '9'))
+		{	
+			a = '2';
+		}
+		new1 = a + 2;
+		new2 = a*3;
+		new3 = a - 1;
+		new4 = a + 4;
+		hash = new1 + new2 + new3 + new4;
+		output += hash;
 	}
+	
+	if (output.size() > 20)
+	output = output.substr(0, 20);
+
 	cout << "Rezultatas: ";
 	cout << output;
 }
