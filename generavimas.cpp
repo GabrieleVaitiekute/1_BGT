8da5efdcf848f0cf6392bdd96d93b3c789e40e82
Gabriele Vaitiekute <gabriele.vaitiekute5@gmail.com>
2024-09-25 23:05:17 +0300

    ruosimas testavimui

--- generavimas.cpp
+++ generavimas.cpp
@@ -11,19 +11,75 @@ std::uniform_int_distribution<char> dis(33, 126); // ASCII
 
 int main()
 {
-    std::ofstream GenFailas("VienuSkiriasi1.txt");
+    std::ofstream GenFailas("SkirtingosPoros.txt");
+
     if (!GenFailas.is_open())
-        {
-            std::cerr << "Failed to open file." << std::endl;
-            return 1;
-        }
+    {
+        std::cerr << "Failed to open file." << std::endl;
+        return 1;
+    }
 
-    int charKiekisGen = charKiekis(generuoti);
+        /*1 ŽINGSNIS
+        int charKiekisGen = charKiekis(generuoti);
 
-    for(int i = 0; i < charKiekisGen; i++)
+        for(int i = 0; i < charKiekisGen; i++)
         {
             GenFailas << dis(generuoti);
         }
+        */
+
+        
+       /*4 ŽINGSNIS 
+    
+        for(int e = 0; e < 4; e++)//pridetas, kai prireike generuoti poras
+        {
+            int ilgis = 10;
+            if(e == 1)ilgis = 100;
+            if(e == 2)ilgis = 500;
+            if(e == 3)ilgis = 1000;
+            for(int a=0; a<25000; a++)
+            {  
+                std::string RanStringA, RanStringB;
+                for(int i = 0; i < ilgis; i++)// 10, 100, 500, 1000
+                {
+                    char RanCharA = dis(generuoti);
+                    RanStringA += RanCharA;
+
+                    char RanCharB = dis(generuoti);
+                    RanStringB += RanCharB;
+
+                }
+
+                GenFailas << RanStringA << " " << RanStringB <<std::endl;
+            }
+        }*/
+
+
+        /*6 ŽINGSNIS*/
+
+
+            for(int a=0; a<100000; a++)
+            {
+                std::string RanStringA, RanStringB;
+                for(int i = 0; i < 500; i++)
+                {
+                    char RanChar = dis(generuoti);
+                    RanStringA += RanChar;
+                }
+                GenFailas << RanStringA;//isspausdina pirma(A) stringa
+
+                RanStringB = RanStringA;//pirma prilygina antram
+
+                char Bpirmas = RanStringA[0] + 2;// Pirmas A stringo simbolis pajudinamas per du, veliau bus priskirtas B strino pirmam simboliui
+
+                if (Bpirmas > 126) Bpirmas = 33 + (Bpirmas - 127);//isitikinama kad judinant per du neiseinama uz acii ribu
+                
+                RanStringB[0] = Bpirmas;// Pajudintas pirmas A stringo simbolis priskirtas B pradziai
+                
+                GenFailas<< " " <<RanStringB;//Isspausdintas B
+
+                GenFailas<< std::endl;
+            } 
 
     GenFailas.close();
 
